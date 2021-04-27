dbef1f172f6826090f5b7a9873ee0c7243760886
TIAGO SILVA <frost.tiago@gmail.com>
2021-04-27 15:39:19 -0300

    Adicionar arquivos de projeto.

---
+++ EtecArrayDeObjetos/Pessoa.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroducaoOO
+{
+    //Especificação de como o "Objeto" será
+    // Classe abstrata não pode ter instância, serve como meio comum.
+    abstract class Pessoa
+    {
+        // atributo (campo)
+        private static int instancias = 0;
+
+
+        // Atributos/Propriedades (Caracteristicas)
+        double altura;
+        double peso;    // private ou sem nada, apenas dentro da própria classe
+        protected int idade;   // public é acessível por todos (dentro e fora da classe)
+        protected string nome; // protected é acessível apenas as Classes FILHAS
+
+
+        // Propriedade (regula o acesso [leitura e escrita] aos campos/atributos)
+        public static int Instancias
+        {
+            get { return instancias; } // Define Instancias como somente leitura.
+        }
+
+
+        public string Nome
+        {
+            get { return nome; } // Define Nome como somente leitura, pois tem apenas o get.
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; } // Define que é possívle gravar valores
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+
+
+        // Método Construtor
+        public Pessoa(string _nome)
+        {
+            //Console.WriteLine("Chamou o método construtor assim que criou o objeto");
+
+            nome = _nome;
+
+            // instancias++;
+            instancias = instancias + 1;
+        }
+
+
+        // Métodos (Ações)
+        public void Respirar()
+        {
+            // programar as ações.
+            Console.WriteLine("A pessoa {0} está respirando.", nome);
+        }
+
+        // Adicionando um método com Parâmetro
+        public void Comer(string _comida)
+        {
+            Console.WriteLine("A pessoa {0} está comendo {1}.", nome, _comida);
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine("A pessoa {0} está andando", nome);
+        }
+    }
+}
---
+++ EtecArrayDeObjetos/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using IntroducaoOO;
+
+namespace EtecArrayDeObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Array de objetos = lista de vários objetos do mesmo tipo.
+             *    i    |   0    |    1    |    2     |     3     |   4   | .....
+             * alunos  |  Kevin | Rafaela |  João    |  Gabriel  |
+             */
+            List<Aluno> alunos = new List<Aluno>();
+
+            
+            //Aluno a1 = new Aluno("Kevin");
+            //alunos.Add(a1);
+            
+
+            alunos.Add(new Aluno("Kevin"));
+            alunos.Add(new Aluno("Rafaela"));
+            alunos.Add(new Aluno("João"));
+
+            Console.WriteLine("Temos {0} alunos no sistema", alunos.Count);
+
+
+            alunos.Add(new Aluno("Gabriel")
+            {
+                Idade = 16,
+                Rm = 789456,
+                Altura = 1.80,
+                Peso = 70,
+            });
+
+
+            Console.WriteLine("Posição 3: ");
+            Console.WriteLine("Nome: {0} ",   alunos[3].Nome);
+            Console.WriteLine("Idade: {0} ",  alunos[3].Idade);
+            Console.WriteLine("Rm: {0} ",     alunos[3].Rm);
+            Console.WriteLine("Altura: {0} ", alunos[3].Altura);
+            Console.WriteLine("Peso: {0} ",   alunos[3].Peso);
+
+            Console.WriteLine("");
+
+
+
+
+            Console.WriteLine("Percorrendo com um Looping For");
+            
+            for(int i=0; i < alunos.Count; i++)
+            {
+                Console.WriteLine("Posição {0}: ", i);
+
+                Console.WriteLine("Nome: {0} ",   alunos[i].Nome);
+                Console.WriteLine("Idade: {0} ",  alunos[i].Idade);
+                Console.WriteLine("Rm: {0} ",     alunos[i].Rm);
+                Console.WriteLine("Altura: {0} ", alunos[i].Altura);
+                Console.WriteLine("Peso: {0} ",   alunos[i].Peso);
+
+                Console.WriteLine("");
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Percorrendo com um Looping Foreach");
+
+            foreach(Aluno item in alunos)
+            {
+                Console.WriteLine("Nome: {0} ",   item.Nome);
+                Console.WriteLine("Idade: {0} ",  item.Idade);
+                Console.WriteLine("Rm: {0} ",     item.Rm);
+                Console.WriteLine("Altura: {0} ", item.Altura);
+                Console.WriteLine("Peso: {0} ",   item.Peso);
+
+                Console.WriteLine("");
+            }
+
+            /**
+             * Insrindo os dados de um aluno
+             */
+            Console.WriteLine("Informe o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Aluno x = new Aluno(nome);
+
+            Console.WriteLine("Informe a Idade do aluno: ");
+            x.Idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o Rm do aluno: ");
+            x.Rm = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o Altura do aluno: ");
+            x.Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o Peso do aluno: ");
+            x.Peso = Convert.ToDouble(Console.ReadLine());
+
+
+            // add na lista de alunos
+            alunos.Add(x);
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Percorrendo com um Looping For");
+
+            for (int i = 0; i < alunos.Count; i++)
+            {
+                Console.WriteLine("Posição {0}: ", i);
+
+                Console.WriteLine("Nome: {0} ", alunos[i].Nome);
+                Console.WriteLine("Idade: {0} ", alunos[i].Idade);
+                Console.WriteLine("Rm: {0} ", alunos[i].Rm);
+                Console.WriteLine("Altura: {0} ", alunos[i].Altura);
+                Console.WriteLine("Peso: {0} ", alunos[i].Peso);
+
+                Console.WriteLine("");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
